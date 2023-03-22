a8d4b1bba6faeb30c2df45eeeae69ae56a036dfe
NikclausCarlon <you@example.com>
2023-03-23 00:28:03 +0300

    add client, server and models with db

---
+++ src/test/java/com/example/docker_hw/DockerHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
