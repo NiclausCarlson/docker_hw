68a03d252d88b552fa4f302d4a4a148cf6cf9a8e
NikclausCarlon <you@example.com>
2023-03-24 23:47:41 +0300

    add forgotten file

---
+++ server/src/main/java/logic/UpdatePrices.java
@@ -0,0 +1,16 @@
+package logic;
+
+public class UpdatePrices {
+    private static long startTime = System.currentTimeMillis();
+    private static final long KUpdateInterval = 2*60*1000; // two minutes
+
+    public static boolean check(){
+        long currentTime = System.currentTimeMillis();
+        if(currentTime - startTime >= KUpdateInterval){
+            startTime = System.currentTimeMillis();
+            return true;
+        }
+        return false;
+    }
+
+}
